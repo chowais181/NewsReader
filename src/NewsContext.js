60def0465319b8b8231a1c7e8f6d57c17ffbbfc8
chowais181 <f180269@nu.edu.pk>
2023-02-20 22:14:45 +0500

    Intial commit

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -5,15 +5,14 @@ export const NewsContext = createContext();
 
 export const NewsContextProvider = (props) => {
   const [data, setData] = useState();
-  const apiKey = "d3a68d3a93a54948a016a1553bc4d20c";
+  const apiKey = "1ea9fe2361ce4546b66d5bd09495cb3e";
+  const BASE_URL = "https://newsapi.org/v2/top-headlines?language=en&apiKey";
 
   useEffect(() => {
     axios
-      .get(
-        `http://newsapi.org/v2/everything?q=rich&from=2020-07-19&sortBy=publishedAt&apiKey=${apiKey}`
-      )
+      .get(` ${BASE_URL}=${apiKey}`)
       .then((response) => setData(response.data))
-      .catch((error) => console.log(error));
+      .catch((error) => console.log("Error: ", error));
   }, []);
 
   return (
--- src/components/News.js
+++ src/components/News.js
@@ -2,13 +2,136 @@ import React, { useContext } from "react";
 import { NewsContext } from "../NewsContext";
 import NewsArticle from "./NewsArticle";
 
+// <---------------- grid and select -------------->
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+// <---------------- grid and select -------------->
+import { useTheme } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Chip from "@mui/material/Chip";
+// <---------------- grid and select -------------->
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+// ------------------------------->
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  "Oliver Hansen",
+  "Van Henry",
+  "April Tucker",
+  "Ralph Hubbard",
+  "Omar Alexander",
+  "Carlos Abbott",
+  "Miriam Wagner",
+  "Bradley Wilkerson",
+  "Virginia Andrews",
+  "Kelly Snyder",
+];
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+// ----------------------------
 function News(props) {
   const { data } = useContext(NewsContext);
-  console.log(data);
+
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState([]);
+
+  console.log(personName);
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
 
   return (
     <div>
       <h1 className="head__text">News App 👋</h1>
+      <hr />
+      <br />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <Item>
+              {" "}
+              <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel id="demo-multiple-chip-label">
+                  Filter By Author
+                </InputLabel>
+                <Select
+                  labelId="demo-multiple-chip-label"
+                  id="demo-multiple-chip"
+                  multiple
+                  value={personName}
+                  onChange={handleChange}
+                  input={
+                    <OutlinedInput
+                      id="select-multiple-chip"
+                      label="Filter By Author"
+                    />
+                  }
+                  renderValue={(selected) => (
+                    <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                      {selected.map((value) => (
+                        <Chip key={value} label={value} />
+                      ))}
+                    </Box>
+                  )}
+                  MenuProps={MenuProps}
+                >
+                  {names.map((name) => (
+                    <MenuItem
+                      key={name}
+                      value={name}
+                      style={getStyles(name, personName, theme)}
+                    >
+                      {name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+        </Grid>
+      </Box>
       <div className="all__news">
         {data
           ? data.articles.map((news) => (
